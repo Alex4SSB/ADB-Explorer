4f1d01e1c74746c9903197dc16f2688bb9be7e2d
Alex <alex4ssb@gmail.com>
2021-06-08 22:03:18 +0300

    Basic file list setup

--- ADB Explorer/Views/ExplorerPage.xaml.cs
+++ ADB Explorer/Views/ExplorerPage.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System.ComponentModel;
+﻿using ADB_Explorer.Contracts.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Controls;
 
 namespace ADB_Explorer.Views
 {
-    public partial class ExplorerPage : Page, INotifyPropertyChanged
+    public partial class ExplorerPage : Page, INotifyPropertyChanged, INavigationAware
     {
         public ExplorerPage()
         {
@@ -13,6 +19,21 @@ namespace ADB_Explorer.Views
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public IEnumerable<string> WindowsFileList { get; set; }
+
+        public void OnNavigatedTo(object parameter)
+        {
+            var files = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+            WindowsFileList = files.Select(f => Path.GetFileName(f));
+
+            //WindowsExplorerView.items
+        }
+
+        public void OnNavigatedFrom()
+        {
+        }
 
         private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
         {
