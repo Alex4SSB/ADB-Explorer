6e39bad9bbb8bd3ff7eea003d814e68b7a16240e
Alex <alex4ssb@gmail.com>
2021-06-10 21:00:26 +0300

    Created a class to handle files for the table

---
+++ ADB Explorer/Models/FileClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace ADB_Explorer.Models
+{
+    public class FileClass
+    {
+        public object Icon { get; }
+
+        private string filePath;
+        public string FilePath { get { return filePath; } }
+
+        public string Name { get; }
+
+        private DateTime date;
+        public string Date { get; }
+
+        private FileType type;
+        public string Type { get; }
+
+        private long size;
+        public string Size { get; }
+
+        public FileClass()
+        {
+        }
+
+        public FileClass(string path, FileType type = FileType.File)
+        {
+            filePath = path;
+
+            Name = type switch
+            {
+                FileType.Drive => path,
+                FileType.Folder => Path.GetFileName(path),
+                FileType.File => Path.GetFileNameWithoutExtension(path),
+                FileType.Parent => "..",
+                _ => throw new NotImplementedException()
+            };
+
+            this.type = type;
+            Type = type.ToString();
+        }
+    }
+
+    public enum FileType
+    {
+        Drive,
+        Folder,
+        File,
+        Parent
+    }
+}
--- ADB Explorer/Views/ExplorerPage.xaml.cs
+++ ADB Explorer/Views/ExplorerPage.xaml.cs
@@ -7,6 +7,8 @@ using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Controls;
+using ADB_Explorer.Models;
+using System.Windows.Input;
 
 namespace ADB_Explorer.Views
 {
@@ -20,16 +22,13 @@ namespace ADB_Explorer.Views
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public List<string> WindowsFileList { get; set; }
+        public List<FileClass> WindowsFileList { get; set; }
 
         public void OnNavigatedTo(object parameter)
         {
-            //var files = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
             var files = DriveInfo.GetDrives();
-            WindowsFileList = files.Select(f => f.Name).ToList();
-            //WindowsFileList = files.Select(f => Path.GetFileName(f));
-
-            //WindowsExplorerGrid.items
+            WindowsFileList = files.Select(f => new FileClass(f.Name, FileType.Drive)).ToList();
+            WindowsExplorerGrid.ItemsSource = WindowsFileList;
         }
 
         public void OnNavigatedFrom()
@@ -51,28 +50,40 @@ namespace ADB_Explorer.Views
 
         private void WindowsExplorerView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            var itemName = WindowsExplorerGrid.SelectedItem?.ToString();
-            if (itemName == "..")
-            {
-                itemName = Directory.GetParent(WindowsFileList[1]).FullName;
-                var parent = Directory.GetParent(itemName);
-                if (parent is null)
-                {
-                    WindowsFileList.Clear();
-                    WindowsFileList.AddRange(DriveInfo.GetDrives().Select(f => f.Name).ToList());
-                    WindowsExplorerGrid.Items.Refresh();
-                    return;
-                }
-                itemName = parent.FullName;
-            }
-            else if (!Directory.Exists(itemName)) return;
-            WindowsFileList.Clear();
-            WindowsFileList.Add("..");
-            WindowsFileList.AddRange(Directory.GetDirectories(itemName));
-            WindowsFileList.AddRange(Directory.GetFiles(itemName));
-            WindowsExplorerGrid.Items.Refresh();
+            //var itemName = WindowsExplorerGrid.SelectedItem?.ToString();
+            //if (itemName == "..")
+            //{
+            //    itemName = Directory.GetParent(WindowsFileList[1]).FullName;
+            //    var parent = Directory.GetParent(itemName);
+            //    if (parent is null)
+            //    {
+            //        WindowsFileList.Clear();
+            //        WindowsFileList.AddRange(DriveInfo.GetDrives().Select(f => f.Name).ToList());
+            //        WindowsExplorerGrid.Items.Refresh();
+            //        return;
+            //    }
+            //    itemName = parent.FullName;
+            //}
+            //else if (!Directory.Exists(itemName)) return;
+            //WindowsFileList.Clear();
+            //WindowsFileList.Add("..");
+            //WindowsFileList.AddRange(Directory.GetDirectories(itemName));
+            //WindowsFileList.AddRange(Directory.GetFiles(itemName));
+            //WindowsExplorerGrid.Items.Refresh();
             //WindowsExplorerGrid.Items.Clear();
             //WindowsFileList.ForEach(i => WindowsExplorerGrid.Items.Add(i));
+        }
+
+        private void DataGridRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.Source is DataGridRow row && row.Item is FileClass file)
+            {
+                WindowsFileList.Clear();
+                WindowsFileList.Add(new(Directory.GetParent(file.FilePath)?.FullName, FileType.Parent));
+                WindowsFileList.AddRange(Directory.GetDirectories(file.FilePath).Select(d => new FileClass(d, FileType.Folder)));
+                WindowsFileList.AddRange(Directory.GetFiles(file.FilePath).Select(f => new FileClass(f, FileType.File)));
+                WindowsExplorerGrid.Items.Refresh();
+            }
         }
     }
 }
