a41af1f9f37b9828480b6526517f906434cecab9
Tal Sarid <sarid.tal@gmail.com>
2021-06-13 01:03:35 +0300

    Fixed size computation to base 1024 and added one fractional digit

--- ADB Explorer/Converters/SizeConverter.cs
+++ ADB Explorer/Converters/SizeConverter.cs
@@ -4,21 +4,12 @@ namespace ADB_Explorer.Converters
 {
     public static class SizeConverter
     {
-        private static readonly string[] logs = { "", "K", "M", "G" };
+        private static readonly string[] scale_table = { "", "K", "M", "G", "T", "P", "E" };
 
-        public static string ToSize(this ulong num) => ((double)num).ToSize();
-
-        public static string ToSize(this double num)
+        public static string ToSize(this UInt64 bytes)
         {
-            int n = 0;
-
-            while (num >= 1000 && n < logs.Length - 1)
-            {
-                num /= 1000;
-                n++;
-            }
-
-            return $"{Math.Round(num)}{logs[n]}B";
+            int scale = (bytes == 0) ? 0 : Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024))); // 0 <= scale <= 6
+            return $"{Math.Round(bytes / Math.Pow(1024, scale), 1)}{scale_table[scale]}B";
         }
     }
 }
