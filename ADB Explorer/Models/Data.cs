e720d0c826c73f5fd9305c8c65e36faf58d98a89
Alex <alex4ssb@gmail.com>
2021-06-14 23:58:15 +0300

    Device name and file list are now static

---
+++ ADB Explorer/Models/Data.cs
@@ -0,0 +1,22 @@
+﻿using ADB_Explorer.Core.Services;
+using System.Collections.Generic;
+
+namespace ADB_Explorer.Models
+{
+    public static class Data
+    {
+        private static string deviceName = "";
+        public static string DeviceName
+        {
+            get
+            {
+                if (deviceName == "")
+                    deviceName = ADBService.GetDeviceName();
+
+                return deviceName;
+            }
+        }
+
+        public static List<FileClass> AndroidFileList { get; set; }
+    }
+}
--- ADB Explorer/Views/ExplorerPage.xaml.cs
+++ ADB Explorer/Views/ExplorerPage.xaml.cs
@@ -4,12 +4,11 @@ using ADB_Explorer.Core.Services;
 using ADB_Explorer.Models;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Controls;
 using System.Windows.Input;
+using static ADB_Explorer.Models.Data;
 
 namespace ADB_Explorer.Views
 {
@@ -26,30 +25,28 @@ namespace ADB_Explorer.Views
         public event PropertyChangedEventHandler PropertyChanged;
 
         public List<FileClass> WindowsFileList { get; set; }
-        public List<FileClass> AndroidFileList { get; set; }
 
         public void OnNavigatedTo(object parameter)
         {
             // Get device name
-            TitleBlock.Text = ADBService.GetDeviceName();
+            TitleBlock.Text = DeviceName;
 
             // Windows
-            WindowsFileList = DriveInfo.GetDrives().Select(f => FileClass.GenerateWindowsFile(f.Name, FileStat.FileType.Drive)).ToList();
+            //WindowsFileList = DriveInfo.GetDrives().Select(f => FileClass.GenerateWindowsFile(f.Name, FileStat.FileType.Drive)).ToList();
 
             PathBox.Text = INTERNAL_STORAGE;
             // Android
-            AndroidFileList = ADBService.ReadDirectory(INTERNAL_STORAGE).Select(f => FileClass.GenerateAndroidFile(f)).ToList();
+            if (AndroidFileList is null)
+                AndroidFileList = ADBService.ReadDirectory(INTERNAL_STORAGE).Select(f => FileClass.GenerateAndroidFile(f)).ToList();
 
             ExplorerGrid.ItemsSource = AndroidFileList;// WindowsFileList;
         }
-
-        
 
         public void OnNavigatedFrom()
         {
         }
 
-        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        private void Set<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
         {
             if (Equals(storage, value))
             {
@@ -62,41 +59,10 @@ namespace ADB_Explorer.Views
 
         private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-        private void WindowsExplorerView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            //var itemName = WindowsExplorerGrid.SelectedItem?.ToString();
-            //if (itemName == "..")
-            //{
-            //    itemName = Directory.GetParent(WindowsFileList[1]).FullName;
-            //    var parent = Directory.GetParent(itemName);
-            //    if (parent is null)
-            //    {
-            //        WindowsFileList.Clear();
-            //        WindowsFileList.AddRange(DriveInfo.GetDrives().Select(f => f.Name).ToList());
-            //        WindowsExplorerGrid.Items.Refresh();
-            //        return;
-            //    }
-            //    itemName = parent.FullName;
-            //}
-            //else if (!Directory.Exists(itemName)) return;
-            //WindowsFileList.Clear();
-            //WindowsFileList.Add("..");
-            //WindowsFileList.AddRange(Directory.GetDirectories(itemName));
-            //WindowsFileList.AddRange(Directory.GetFiles(itemName));
-            //WindowsExplorerGrid.Items.Refresh();
-            //WindowsExplorerGrid.Items.Clear();
-            //WindowsFileList.ForEach(i => WindowsExplorerGrid.Items.Add(i));
-        }
-
         private void DataGridRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (e.Source is DataGridRow row && row.Item is FileClass file && file.Type != FileStat.FileType.File)
             {
-                // WindowsFileList.Clear();
-                // WindowsFileList.Add(new(Directory.GetParent(file.Path)?.FullName, FileStat.FileType.Parent));
-                // WindowsFileList.AddRange(Directory.GetDirectories(file.Path).Select(d => new PhysicalFileClass(d, FileStat.FileType.Folder)));
-                // WindowsFileList.AddRange(Directory.GetFiles(file.Path).Select(f => new PhysicalFileClass(f, FileStat.FileType.File)));
-
                 PathBox.Text = file.Path;
 
                 AndroidFileList.Clear();
