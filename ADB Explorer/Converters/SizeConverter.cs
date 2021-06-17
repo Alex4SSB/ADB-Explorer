551c7774146a764d79ea80ce9fbef97f538ce973
Alex <alex4ssb@gmail.com>
2021-06-17 22:32:10 +0300

    Changed decimal point for file size

--- ADB Explorer/Converters/SizeConverter.cs
+++ ADB Explorer/Converters/SizeConverter.cs
@@ -9,7 +9,8 @@ namespace ADB_Explorer.Converters
         public static string ToSize(this UInt64 bytes)
         {
             int scale = (bytes == 0) ? 0 : Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024))); // 0 <= scale <= 6
-            return $"{Math.Round(bytes / Math.Pow(1024, scale), 1)}{scale_table[scale]}B";
+            double value = bytes / Math.Pow(1024, scale);
+            return $"{(Math.Round(value, value < 100 ? 1 : 0))}{scale_table[scale]}B";
         }
     }
 }
