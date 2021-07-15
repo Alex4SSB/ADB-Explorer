069a632e71e359f4f69504b0231d0dcf67cab768
Tal Sarid <sarid.tal@gmail.com>
2021-07-15 22:58:38 +0300

    AdbProgressRedirection: 1) Added standard error messages 2) Improved scope guards

--- AdbProgressRedirection/main.cpp
+++ AdbProgressRedirection/main.cpp
@@ -12,13 +12,18 @@
 
 static const COORD origin_coord = { 0, 0 };
 
+template<typename T, typename D>
+auto MakeScopeFinallyGuard(T* data, D&& deleter) { return std::unique_ptr<T, D>(data, std::forward<D>(deleter)); }
+
 HANDLE CreateInheritableConsoleHandle() {
 	SECURITY_ATTRIBUTES security_attrs;
 	security_attrs.nLength = sizeof(security_attrs);
 	security_attrs.lpSecurityDescriptor = NULL;
 	security_attrs.bInheritHandle = TRUE;
 	return CreateConsoleScreenBuffer(
-		GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, &security_attrs, CONSOLE_TEXTMODE_BUFFER, NULL);
+		GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE,
+		&security_attrs,
+		CONSOLE_TEXTMODE_BUFFER, NULL);
 }
 
 std::wstring ReadConsoleLine(HANDLE console_handle, SHORT line_offset) {
@@ -57,7 +62,8 @@ HANDLE CreateChildProcess(const std::wstring& cmd_line) {
 	ZeroMemory(&startup_info, sizeof(startup_info));
 	startup_info.cb = sizeof(startup_info);
 	startup_info.dwFlags = STARTF_FORCEOFFFEEDBACK;
-	if (!CreateProcess(NULL, const_cast<LPWSTR>(cmd_line.c_str()), NULL, NULL, TRUE, 0, NULL, NULL, &startup_info, &proc_info))
+	if (!CreateProcess(
+		NULL, const_cast<LPWSTR>(cmd_line.c_str()), NULL, NULL, TRUE, 0, NULL, NULL, &startup_info, &proc_info))
 	{
 		return INVALID_HANDLE_VALUE;
 	}
@@ -73,6 +79,7 @@ bool IsLinePrintable(const std::wstring& line) {
 int wmain(int argc, wchar_t* argv[], wchar_t* envp[]) {
 	// If no command to execute
 	if (argc <= 1) {
+		std::wcerr << L"No arguments to execute were provided!" << std::endl;
 		return -1;
 	}
 
@@ -83,42 +90,46 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[]) {
 	fflush(stdout);
 	int previous_stdout_mode = _setmode(_fileno(stdout), _O_U16TEXT);
 	if (previous_stdout_mode == -1) {
-		return -2;
+		std::wcerr << L"Couldn't set STDOUT mode to UTF16!" << std::endl;
+		return -1;
 	}
 
-	// RAII guard to restore stdout's previous mode
 #pragma warning(push)
 #pragma warning(disable: 6031)
+	// RAII guard to restore stdout's previous mode
 	auto stdout_mode_restore = [](int* m) { _setmode(_fileno(stdout), *m); };
+	auto stdout_mode_guard = MakeScopeFinallyGuard(&previous_stdout_mode, stdout_mode_restore);
 #pragma warning(pop)
-	std::unique_ptr<int, decltype(stdout_mode_restore)> stdout_mode_guard(&previous_stdout_mode, stdout_mode_restore);
 
 	// Create a new console screen buffer
 	HANDLE console_handle = CreateInheritableConsoleHandle();
 	if (console_handle == INVALID_HANDLE_VALUE) {
-		return -3;
+		std::wcerr << L"Couldn't create a console screen buffer!" << std::endl;
+		return -1;
 	}
 
 	// RAII guard that will close the console handle automatically
 	auto handle_closer = [](HANDLE* h) { CloseHandle(*h); };
-	std::unique_ptr<HANDLE, decltype(handle_closer)> stdout_handle_guard(&console_handle, handle_closer);
+	auto stdout_handle_guard = MakeScopeFinallyGuard(&console_handle, handle_closer);
 
 	// Create a child process with inherited console screen buffer as its stdout
 	HANDLE child_handle;
 	{
 		// Replace stdout with the created console buffer so the child process will inherite it as its stdout
 		if (!SetStdHandle(STD_OUTPUT_HANDLE, console_handle)) {
-			return -4;
+			std::wcerr << L"Couldn't set STDOUT to the created console screen buffer!" << std::endl;
+			return -1;
 		}
 
 		// RAII guard that will restore stdout to the original one
 		auto stdout_restore = [](HANDLE* h) { SetStdHandle(STD_OUTPUT_HANDLE, *h); };
-		std::unique_ptr<HANDLE, decltype(stdout_restore)> stdout_restore_guard(&stdout_handle, stdout_restore);
+		auto stdout_restore_guard = MakeScopeFinallyGuard(&stdout_handle, stdout_restore);
 
 		// Set our created console to UTF-16
 		fflush(stdout);
 		if (_setmode(_fileno(stdout), _O_U16TEXT) == -1) {
-			return -5;
+			std::wcerr << L"Couldn't set the created console screen buffer's mode to UTF16!" << std::endl;
+			return -1;
 		}
 
 		// Build child process command line for execution
@@ -132,12 +143,13 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[]) {
 		// Start child process
 		child_handle = CreateChildProcess(command_line);
 		if (child_handle == INVALID_HANDLE_VALUE) {
-			return -6;
+			std::wcerr << L"Couldn't create a child process for: " << command_line << std::endl;
+			return -1;
 		}
 	}
 
 	// RAII guard that will close the child handle automatically
-	std::unique_ptr<HANDLE, decltype(handle_closer)> child_handle_guard(&child_handle, handle_closer);
+	auto child_handle_guard = MakeScopeFinallyGuard(&child_handle, handle_closer);
 
 	// While the child process is running
 	std::wstring prev_line_str;
@@ -160,7 +172,8 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[]) {
 	// Fetch exit code from the child process to return it
 	DWORD exit_code;
 	if (!GetExitCodeProcess(child_handle, &exit_code)) {
-		return -8;
+		std::wcerr << L"Couldn't get child process return code!" << std::endl;
+		return -1;
 	}
 
 	return exit_code;
