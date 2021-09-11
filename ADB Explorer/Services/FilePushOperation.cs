8e48abba19c714419140f5224e96154472048769
Tal Sarid <sarid.tal@gmail.com>
2021-09-11 20:28:44 +0300

    Added FilePushOperation and FilePullOperation classes to handle async adb push/pull internally

---
+++ ADB Explorer/Services/FilePushOperation.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Threading;
+
+namespace ADB_Explorer.Services
+{
+    public class FilePushOperation : FileSyncOperation
+    {
+        public FilePushOperation(Dispatcher dispatcher, ADBService.Device adbDevice, string sourcePath, string targetPath)
+            : base(dispatcher, adbDevice.PushFile, adbDevice, sourcePath, targetPath) {}
+    }
+}
