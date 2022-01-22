9fc21c837f6746736b3ff9c310f9f35141bc4dd2
Alex <alex4ssb@gmail.com>
2022-01-23 00:48:01 +0200

    Added fluent expander style for settings

---
+++ ADB Explorer/Converters/MultiplyConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ADB_Explorer.Converters
+{
+    public class MultiplyConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double result = 1.0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] is double val)
+                    result *= val;
+            }
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
