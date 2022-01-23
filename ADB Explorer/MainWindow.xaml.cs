77eea4cf7c3b181f3419e95d0eb4a54df5310da5
Alex <alex4ssb@gmail.com>
2022-01-24 01:17:46 +0200

    Implemented selective fluent style to the settings panel

--- ADB Explorer/MainWindow.xaml.cs
+++ ADB Explorer/MainWindow.xaml.cs
@@ -418,31 +418,31 @@ namespace ADB_Explorer
             PendingOperationsDataGrid.ItemsSource = fileOperationQueue.PendingOperations;
             CompletedOperationsDataGrid.ItemsSource = fileOperationQueue.CompletedOperations;
 
-            InitColumnConfig();
+            //InitColumnConfig();
         }
 
-        private void InitColumnConfig()
-        {
-            foreach (var type in (ColumnType[]) Enum.GetValues(typeof(ColumnType)))
-            {
-                var possibleColumns = POSSIBLE_COLUMNS.Where(c => c.IsType(type)).Select(c => c.Name).ToList();
-                List<string> requestedColumns = new();
-                if (Storage.RetrieveValue(type) is List<string> cols)
-                    requestedColumns.AddRange(cols);
+        //private void InitColumnConfig()
+        //{
+        //    foreach (var type in (ColumnType[]) Enum.GetValues(typeof(ColumnType)))
+        //    {
+        //        var possibleColumns = POSSIBLE_COLUMNS.Where(c => c.IsType(type)).Select(c => c.Name).ToList();
+        //        List<string> requestedColumns = new();
+        //        if (Storage.RetrieveValue(type) is List<string> cols)
+        //            requestedColumns.AddRange(cols);
 
-                var configs = requestedColumns?.Select(item => new ColumnConfig(selected: item, columnList: possibleColumns)).ToList();
-                configs.Add(new(selected: "[Add]",columnList: possibleColumns, isExisting: false));
+        //        var configs = requestedColumns?.Select(item => new ColumnConfig(selected: item, columnList: possibleColumns)).ToList();
+        //        configs.Add(new(selected: "[Add]",columnList: possibleColumns, isExisting: false));
 
-                ColumnConfig columns = new()
-                {
-                    Title = $"{type} Operation{(type is ColumnType.Current ? "" : "s")}",
-                    Items = configs
-                };
-                ColumnConfigs.Add(columns);
-            }
+        //        ColumnConfig columns = new()
+        //        {
+        //            Title = $"{type} Operation{(type is ColumnType.Current ? "" : "s")}",
+        //            Items = configs
+        //        };
+        //        ColumnConfigs.Add(columns);
+        //    }
 
-            FileOpSettingsTree.ItemsSource = ColumnConfigs;
-        }
+        //    FileOpSettingsTree.ItemsSource = ColumnConfigs;
+        //}
 
         private void InitDevice()
         {
@@ -1088,7 +1088,7 @@ namespace ADB_Explorer
                 IsFolderPicker = true,
                 Multiselect = false
             };
-            if (DefaultFolderBlock.Text != "[not set]")
+            if (DefaultFolderBlock.Text != "")
                 dialog.DefaultDirectory = DefaultFolderBlock.Text;
 
             if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
