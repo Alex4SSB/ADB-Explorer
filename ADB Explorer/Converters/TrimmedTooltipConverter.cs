d007bb15dc7246aabd69ce26068a6baf89eee9cb
Alex <alex4ssb@gmail.com>
2022-06-15 22:26:56 +0300

    Fixed #110 - Explorer item cells can now display text tooltip • Text tooltips are now only displayed when text is longer than the container

---
+++ ADB Explorer/Converters/TrimmedTooltipConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ADB_Explorer.Converters
+{
+    public class TrimmedTooltipConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is null)
+                return Visibility.Collapsed;
+
+            // FrameworkElement to include both TextBlock and TextBox
+            var textBlock = value as FrameworkElement;
+
+            textBlock.Measure(new(double.PositiveInfinity, double.PositiveInfinity));
+            var margin = textBlock.Margin.Left + textBlock.Margin.Right;
+
+            return textBlock.ActualWidth + margin < textBlock.DesiredSize.Width
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
