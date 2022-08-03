dd1d6abe7c6954363ea7337ac72c509500409abb
Alex <alex4ssb@gmail.com>
2022-08-03 23:49:16 +0300

    • Fixed #143 - data grid vertical scroll viewer is now expanded into the header area • Redesigned data grid column header sort icon • Prepared data grid column filter button • Fixed file op first column tooltip display time • Added explorer grid first column tooltip

---
+++ ADB Explorer/Helpers/Attachable/DataGridHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace ADB_Explorer.Helpers
+{
+    public static class DataGridHelper
+    {
+        public static bool GetIsFilteringEnabled(DataGridColumnHeader control) =>
+            (bool)control.GetValue(IsFilteringEnabledProperty);
+
+        public static void SetIsFilteringEnabled(DataGridColumnHeader control, bool value) =>
+            control.SetValue(IsFilteringEnabledProperty, value);
+
+        public static readonly DependencyProperty IsFilteringEnabledProperty =
+            DependencyProperty.RegisterAttached(
+                "IsFilteringEnabled",
+                typeof(bool),
+                typeof(DataGridHelper),
+                null);
+    }
+}
