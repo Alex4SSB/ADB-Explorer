e13bb60ba0391732a6563cf1bcb21041f68c0762
Alex4SSB <Alex4SSB@gmail.com>
2025-02-01 23:20:25 +0200

    v0.9.25020 release • Fixed #6 • Fixed #231 • Fixed #234 • Fixed #235

--- ADB Explorer/Properties/Resources.Designer.cs
+++ ADB Explorer/Properties/Resources.Designer.cs
@@ -100,7 +100,7 @@ namespace ADB_Explorer.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.8.24121.
+        ///   Looks up a localized string similar to 0.9.25020.
         /// </summary>
         public static string AppVersion {
             get {
