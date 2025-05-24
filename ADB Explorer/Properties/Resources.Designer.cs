f412f158c27430f1486b358b97d8f76134f3efde
Alex4SSB <Alex4SSB@gmail.com>
2025-05-25 00:05:21 +0300

    v0.9.25051 pre-release

--- ADB Explorer/Properties/Resources.Designer.cs
+++ ADB Explorer/Properties/Resources.Designer.cs
@@ -100,7 +100,7 @@ namespace ADB_Explorer.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.9.25050.
+        ///   Looks up a localized string similar to 0.9.25051.
         /// </summary>
         public static string AppVersion {
             get {
