2d610594e45949a9b3cc6cdf6b7cb17d2d57e4a5
alexSSB <alexssb@live.com>
2025-11-15 17:32:27 +0200

    Bump version

--- ADB Explorer/Properties/AppGlobal.Designer.cs
+++ ADB Explorer/Properties/AppGlobal.Designer.cs
@@ -19,7 +19,7 @@ namespace ADB_Explorer.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class AppGlobal {
@@ -80,7 +80,7 @@ namespace ADB_Explorer.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.9.25110.
+        ///   Looks up a localized string similar to 0.9.25111.
         /// </summary>
         public static string AppVersion {
             get {
