84937d171a29daa3753d2a8e83b50a323ee1fe89
alexSSB <alexssb@live.com>
2026-02-21 21:29:04 +0200

    v0.9.26022 release

--- ADB Explorer/Properties/AppGlobal.Designer.cs
+++ ADB Explorer/Properties/AppGlobal.Designer.cs
@@ -80,7 +80,7 @@ namespace ADB_Explorer.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 0.9.26021.
+        ///   Looks up a localized string similar to 0.9.26022.
         /// </summary>
         public static string AppVersion {
             get {
